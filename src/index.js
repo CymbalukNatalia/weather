9220b2a438272ca88de12b2889ace47d9093c9d9
CymbalukNatalia <57503920+CymbalukNatalia@users.noreply.github.com>
2019-11-19 22:13:13 +0200

    add form

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import * as serviceWorker from './serviceWorker';
 class App extends React.Component {
 	render () {
 		return (
-			<div>
-				Weather
+			<div className="mainPageImage">
+				<h1>Weather</h1>
+				<div className="form-group col-md-6">
+      				<label for="inputCity">City or coordinates</label>
+      				<input type="text" className="form-control" id="inputCity" />
+    			</div>
 			</div>
 
 		);
