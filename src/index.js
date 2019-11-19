3246b744b1c6bdbe3ca88cad93a6404ad7671f82
CymbalukNatalia <57503920+CymbalukNatalia@users.noreply.github.com>
2019-11-19 21:57:49 +0200

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css' 
 import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+	render () {
+		return (
+			<div>
+				Weather
+			</div>
+
+		);
+	}
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
